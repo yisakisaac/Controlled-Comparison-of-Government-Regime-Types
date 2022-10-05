683583b16a781e1b6b4fb5d7635cb3a434405700
yisakisaac <115116705+yisakisaac@users.noreply.github.com>
2022-10-05 14:46:39 -0700

    push script

--- making controlled comparison.R
+++ making controlled comparison.R
@@ -1,7 +1,13 @@
 library(poliscidata)
 
 #cross tabulation analysis of ethnicity and regime (question 3)
-xtp(world,democ_regime,frac_eth3,xlab="ethnic heterogeneity classification",ylab="democracy",main ="relationship of regime type and ethnic heterogeneity")
+xtp(world,
+    democ_regime,
+    frac_eth3,
+    xlab="ethnic heterogeneity classification",
+    ylab="democracy",
+    main ="relationship of regime type and ethnic heterogeneity")
+
 printC(xtp(world,democ_regime,frac_eth3))
 
 #controlled comparison of regime type (question 5)
