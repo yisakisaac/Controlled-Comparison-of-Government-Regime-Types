fd03fa9bdb76ec0bab63bf9ef98d715338c7d4e0
yisakisaac <115116705+yisakisaac@users.noreply.github.com>
2022-10-05 14:46:02 -0700

    Add files via upload

---
+++ making controlled comparison lab 8.R
@@ -0,0 +1,8 @@
+library(poliscidata)
+
+#cross tabulation analysis of ethnicity and regime (question 3)
+xtp(world,democ_regime,frac_eth3,xlab="ethnic heterogeneity classification",ylab="democracy",main ="relationship of regime type and ethnic heterogeneity")
+printC(xtp(world,democ_regime,frac_eth3))
+
+#controlled comparison of regime type (question 5)
+xtabC(~democ_regime+frac_eth3+gdp_cap2,worldD)
